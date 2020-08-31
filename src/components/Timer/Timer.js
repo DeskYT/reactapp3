3d97c43a067c64003d153e151507bb820f2e5b7b
DeskYT <deskytube@gmail.com>
2020-08-31 08:58:17 +0300

    Timer.js Controls Update

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -40,9 +40,8 @@ class Timer extends Component {
         let secs = s % 60; //Секунды (0-59)
         s = (s - secs) / 60; //Приведение секунд в минуты
         let mins = s % 60; //Минуты
-        let hrs = (s - mins) / 60; //Часы
         s = (s-mins) / 60
-        hrs = s % 24; //Часы
+        let hrs = s % 24; //Часы
         let days = (s-hrs) / 24;
         if (days > 0) days = days < 10 ? `0${days}` : days;
         if (hrs) hrs = hrs < 10 ? `0${hrs}` : hrs;
@@ -51,22 +50,25 @@ class Timer extends Component {
         ms = ms < 10 ? `00${ms}` : ms < 100 ? `0${ms}`: ms;
 
         let res = (days ? `${days}:` : "") + (hrs ? `${hrs}:` : "") + (mins ? `${mins}:` : "") + secs + '.' + ms
-        //return hrs + ':' + mins + ':' + secs + '.' + ms
         return res;
     }
 
-    control(){
-        return this.state.isEnabled ? <button onClick={this.stop}>Stop</button> : <button onClick={this.start}>Start</button>
+    controls(){
+        return (
+            <>
+                {this.state.isEnabled ? <button onClick={this.stop}>Stop</button> : <button onClick={this.start}>Start</button>}
+                <button onClick={this.reset}>Reset</button>
+            </>
+        )
     }
 
     render() {
         const {time} = this.state;
         return (
             <div>
-                <h1>Time</h1>
-                {this.control()}
-                <button onClick={this.reset}>Reset</button>
+                <h1>Timer</h1>
                 <h2>{this.msToTime(time)}</h2>
+                {this.controls()}
             </div>
         );
     }
